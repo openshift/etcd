99db8578857b49d110ce39512060db7d7e1bfcb9
Thomas Jungblut <tjungblu@redhat.com>
2025-12-17 13:37:54 +0100

    Instrument watchstream send loop
    This PR adds a few more timing metrics to the watch send loop.
    We're currently trying to track down some sort of contention affecting
    all gRPC endpoints, we see a hard QPS ceiling when the apiserver restarts with
    watch cache inits.
    
    There's currently no metric to help us pinpoint where the time is going
    to create such a ceiling, thus adding some more metrics here.
    
    Somewhat related to #15402.
    
    Signed-off-by: Thomas Jungblut <tjungblu@redhat.com>

--- server/etcdserver/api/v3rpc/watch.go
+++ server/etcdserver/api/v3rpc/watch.go
@@ -405,6 +405,7 @@ func (sws *serverWatchStream) sendLoop() {
 				return
 			}
 
+			start := time.Now()
 			// TODO: evs is []mvccpb.Event type
 			// either return []*mvccpb.Event from the mvcc package
 			// or define protocol buffer with []mvccpb.Event.
@@ -475,11 +476,15 @@ func (sws *serverWatchStream) sendLoop() {
 			}
 			sws.mu.Unlock()
 
+			totalDur := time.Since(start)
+			watchSendLoopWatchStreamTime.Observe(totalDur.Seconds())
+			watchSendLoopWatchStreamTimePerEvent.Observe(totalDur.Seconds() / float64(len(evs)))
+
 		case c, ok := <-sws.ctrlStream:
 			if !ok {
 				return
 			}
-
+			start := time.Now()
 			if err := sws.gRPCStream.Send(c); err != nil {
 				if isClientCtxErr(sws.gRPCStream.Context().Err(), err) {
 					sws.lg.Debug("failed to send watch control response to gRPC stream", zap.Error(err))
@@ -517,7 +522,11 @@ func (sws *serverWatchStream) sendLoop() {
 				delete(pending, wid)
 			}
 
+			watchSendLoopControlStreamTime.Observe(time.Since(start).Seconds())
+
 		case <-progressTicker.C:
+			start := time.Now()
+
 			sws.mu.Lock()
 			for id, ok := range sws.progress {
 				if ok {
@@ -526,6 +535,7 @@ func (sws *serverWatchStream) sendLoop() {
 				sws.progress[id] = true
 			}
 			sws.mu.Unlock()
+			watchSendLoopProgressTime.Observe(time.Since(start).Seconds())
 
 		case <-sws.closec:
 			return
